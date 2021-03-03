c2f2fc4c9144a7a80add0b365f7d8d5e0a089c76
Michael Hanus <mh@informatik.uni-kiel.de>
2021-03-03 12:20:21 +0100

    Extend makefile and script to call REPL with correct package paths

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
 # Makefile to install components of curry2go compiler
 
-# CPM executable
+# The root directory of the installation
+export ROOT = $(CURDIR)
+# The binary directory
+export BINDIR = $(ROOT)/bin
+# Directory where the actual libraries are located
+export LIBDIR = $(ROOT)/lib
+# The CPM executable
 CPM=cypm
 
 # Standard location of Curry2Go run-time auxiliaries
@@ -10,7 +16,7 @@ GOWORKSPACE=$(HOME)/go/src
 COMPILER=$(HOME)/.cpm/bin/curry2go
 
 .PHONY: install
-install: runtime
+install: scripts runtime
 	$(CPM) install
 
 .PHONY: uninstall
@@ -25,6 +31,16 @@ runtime:
 	mkdir -p $(GOWORKSPACE)
 	cp -r gocurry $(GOWORKSPACE)/gocurry
 
+# install the scripts in the bin directory:
+.PHONY: scripts
+scripts:
+	cd scripts && $(MAKE) all
+
+# remove the scripts in the bin directory:
+.PHONY: cleanscripts
+cleanscripts:
+	cd scripts && $(MAKE) clean
+
 # clean compilation targets
 .PHONY: clean
 clean:
@@ -32,6 +48,6 @@ clean:
 
 # clean all installed components
 .PHONY: cleanall
-cleanall: clean
+cleanall: clean cleanscripts
 	/bin/rm -rf $(GOWORKSPACE) $(COMPILER)
 
---
+++ scripts/c2go.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+#
+# Start interactive read-eval-print loop of Curry2Go
+
+CURRY2GOHOME=`echo CURRY2GOHOME must be defined here!`
+export CURRY2GOHOME
+
+# The bin directory of CURRY2GO:
+CURRY2GOBIN=$CURRY2GOHOME/bin
+
+# The directory where CPM installs the binaries:
+CPMBIN="$HOME/.cpm/bin"
+
+# Check whether we should call CPM to compute the correct load path:
+WHICHCPM=`which cypm`
+if [ ! -d "$HOME" ] ; then
+  USECPM=no   # do not use CPM without a home directory
+elif [ -x $CPMBIN/cypm ] ; then
+  CYPMBIN=$CPMBIN/cypm
+  USECPM=yes
+elif [ -x "$WHICHCPM" ] ; then
+  CYPMBIN=$WHICHCPM
+  USECPM=yes
+else
+  USECPM=no
+fi
+
+# Use readline wrapper rlwrap for REPL if rlwrap exists,
+# we have tty as stdin, and we have a home dir to store rlwrap's history:
+USERLWRAP=no
+if tty -s ; then
+  RLWRAP=`which rlwrap`
+  if [ -x "$RLWRAP" -a -d "$HOME" ] ; then
+    USERLWRAP=yes
+  fi
+fi
+
+# check arguments for appropriate settings:
+for i in $* ; do
+  case $i in
+    --help | -h | -\? ) USECPM=no ;;
+    --version | -V    ) USECPM=no ;;
+    --numeric-version | --compiler-name | --base-version ) USECPM=no ;;
+    --nocypm ) USECPM=no ;;
+    --noreadline ) USERLWRAP=no
+  esac
+done
+
+REPL="$CPMBIN/c2go"
+if [ ! -x "$REPL" ] ; then
+  echo "ERROR: executable '$REPL' not found!" >&2
+  echo "Run: cd $CURRY2GOHOME && make" >&2
+  exit 1
+fi
+
+if [ $USECPM = yes ] ; then
+  # set CURRYPATH with 'deps' command of CPM
+  echo "Compute CURRYPATH with '$CYPMBIN'..."
+  CPMPATH=`"$CYPMBIN" -v quiet -d CURRYBIN="$REPL" deps -p`
+  if [ $? -gt 0 ] ; then
+    echo $CPMPATH
+    exit 1
+  fi
+  if [ -n "$CURRYPATH" ] ; then
+    CURRYPATH=$CURRYPATH:$CPMPATH # keep existing CURRYPATH setting
+  else
+    CURRYPATH=$CPMPATH
+  fi
+  export CURRYPATH
+fi
+
+# do not use rlwrap inside emacs:
+if [ "$TERM" = dumb ] ; then
+  USERLWRAP=no
+fi
+
+if [ $USERLWRAP = yes ] ; then
+  exec rlwrap -c "$REPL" ${1+"$@"}
+else
+  exec "$REPL" ${1+"$@"}
+fi
