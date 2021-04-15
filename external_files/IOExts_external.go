c23a20dff672bf0b7272f99048f3907d0b4bc247
stu211735 <stu211735@mail.uni-kiel.de>
2021-04-15 10:34:12 +0200

    Add implementation of IOExts_external. Closes #24

---
+++ external_files/IOExts_external.go
@@ -0,0 +1,74 @@
+package SystemIOExts
+
+import "gocurry"
+import "../IO"
+import "../../Prelude"
+import "os/exec"
+
+
+func ExternalSystem_IOExts_prim_execCmd(task *gocurry.Task){
+    root := task.GetControl()
+    commandNode := root.GetChild(0)
+    command := gocurry.ReadString(commandNode)
+    
+    // create command
+    cmd := exec.Command(command)
+    
+    // get pipes from command
+    stdinPipe, err := cmd.StdinPipe()
+    if(err != nil){
+        panic("System.IOExts.execCmd: " + err.Error())
+    }
+    
+    stdoutPipe, err := cmd.StdoutPipe()
+    if(err != nil){
+        panic("System.IOExts.execCmd: " + err.Error())
+    }
+    
+    stderrPipe, err := cmd.StderrPipe()
+    if(err != nil){
+        panic("System.IOExts.execCmd: " + err.Error())
+    }
+    
+    // create handles for pipes
+    stdinHandle := SystemIO.HandleCreate(root.NewNode(), nil, &stdinPipe, 1, true)
+    stdoutHandle := SystemIO.HandleCreate(root.NewNode(), &stdoutPipe, nil, 0, true)
+    stderrHandle := SystemIO.HandleCreate(root.NewNode(), &stderrPipe, nil, 0, true)
+    
+    // start command
+    cmd.Start()
+    
+    // return
+    tupel := Prelude.Prelude__CREATE_LbCommaCommaCommaRb(root.NewNode(), stdinHandle, stdoutHandle, stderrHandle)
+    gocurry.IOCreate(root, tupel)
+}
+
+func ExternalSystem_IOExts_prim_connectToCmd(task *gocurry.Task){
+    root := task.GetControl()
+    commandNode := root.GetChild(0)
+    command := gocurry.ReadString(commandNode)
+    
+    // create command
+    cmd := exec.Command(command)
+    
+    // get pipes from command
+    stdinPipe, err := cmd.StdinPipe()
+    if(err != nil){
+        panic("System.IOExts.execCmd: " + err.Error())
+    }
+    
+    stdoutPipe, err := cmd.StdoutPipe()
+    if(err != nil){
+        panic("System.IOExts.execCmd: " + err.Error())
+    }
+    
+    
+    // create handles for pipes
+    handle := SystemIO.HandleCreate(root.NewNode(), &stdoutPipe, &stdinPipe, 1, true)
+    
+    // start command
+    cmd.Start()
+    
+    // return
+    gocurry.IOCreate(root, handle)
+}
