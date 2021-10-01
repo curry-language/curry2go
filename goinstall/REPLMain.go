7041e044c9a01a997dc4d0f6563a90dc0a973264
Michael Hanus <mh@informatik.uni-kiel.de>
2021-10-01 15:22:10 +0200

    Fix main calls for separate installation

--- goinstall/REPLMain.go
+++ goinstall/REPLMain.go
@@ -5,6 +5,6 @@ import "curry2go/Curry2Go/REPL"
 
 func main(  )(  ){
     node := Curry2GoREPL.Curry2GoREPL__CREATE_main( new( gocurry.Node ) )
-    gocurry.Evaluate( node, false, false, gocurry.FS, 0, 0 )
+    gocurry.Evaluate( node, false, false, gocurry.FS, 0, 0, 0 )
 }
 
