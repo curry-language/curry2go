0be6b2c26e0743f89c3b7e79a7ac465b287f0802
Michael Hanus <mh@informatik.uni-kiel.de>
2021-11-27 14:22:25 +0100

    Add skeleton for externals in package `socket`

---
+++ external_files/Socket_external.go
@@ -0,0 +1,28 @@
+package Network_DOT_Socket
+
+import "gocurry"
+
+
+func ExternalNetworkDot_SocketDot_primUs_listenOn(task *gocurry.Task){
+    panic("Network.Socket.listenOn: not yet implemented")
+}
+
+func ExternalNetworkDot_SocketDot_listenOnFresh(task *gocurry.Task){
+    panic("Network.Socket.listenOnFresh: not yet implemented")
+}
+
+func ExternalNetworkDot_SocketDot_primUs_socketAccept(task *gocurry.Task){
+    panic("Network.Socket.socketAccept: not yet implemented")
+}
+
+func ExternalNetworkDot_SocketDot_primUs_waitForSocketAccept(task *gocurry.Task){
+    panic("Network.Socket.waitForSocketAccept: not yet implemented")
+}
+
+func ExternalNetworkDot_SocketDot_primUs_sClose(task *gocurry.Task){
+    panic("Network.Socket.sClose: not yet implemented")
+}
+
+func ExternalNetworkDot_SocketDot_primUs_connectToSocket(task *gocurry.Task){
+    panic("Network.Socket.connectToSocket: not yet implemented")
+}
