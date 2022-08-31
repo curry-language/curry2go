79da1440d846a417031dac9992263503298de2de
Michael Hanus <mh@informatik.uni-kiel.de>
2022-08-31 16:51:56 +0200

    Fix main install scripts for Go executables

--- goinstall/CPMMain.go
+++ goinstall/CPMMain.go
@@ -5,6 +5,7 @@ import "curry2go/CPM/Main"
 
 func main(  )(  ){
     node := CPM_DOT_Main.CPM_DOT_Main__CREATE_main( new( gocurry.Node ) )
-    gocurry.Evaluate( node, false, false, false, gocurry.FS, 0, 0, 0 )
+    gocurry.Evaluate( node, false, false, false, gocurry.FS, 0, 0, 0,
+                      false, []string{  } )
 }
 
